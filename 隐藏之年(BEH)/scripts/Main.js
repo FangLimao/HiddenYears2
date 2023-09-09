d5aa992d077c180214dc0a4a563556df9c0e1451
方漓猫 <mucigames@outlook.com>
2023-09-09 14:35:35 +0800

    测试脚本

--- 隐藏之年(BEH)/scripts/Main.js
+++ 隐藏之年(BEH)/scripts/Main.js
@@ -1,3 +1,41 @@
+let HiddenYears = new Object;
+HiddenYears.expGameplay = new Boolean;
+
+import { ActionFormData } from "@minecraft/server-ui";
+import { world } from "@minecraft/server";
+
+if (HiddenYears.expGameplay === true) {
+    let guideBook = new ActionFormData;
+    guideBook.title("向导书");
+    guideBook.body("这里是本模组的向导");
+};
+
+/*暂时弃用，原因同下
+world.beforeEvents.chatSend.subscribe((eventData) => {
+    const player = eventData.sender;
+    switch (eventData.message) {
+        case '/exp':
+            eventData.cancel = true;
+            switch (HiddenYears.expGameplay) {
+                case true:
+                    HiddenYears.expGameplay = false;
+                    player.sendMessage("实验性玩法已关闭");
+                case false:
+                    HiddenYears.expGameplay = true;
+                    player.sendMessage("实验性玩法已开启");
+                default: break;
+            }
+            break;
+        default: break;
+    }
+});
+*/
+
 /*
-WIP
+暂时弃用,当beforeEvents公开到正式版中时才会启用此段代码
+world.beforeEvents.itemUse.subscribe(event => {
+    if (event.itemStack.typeId === "hy:guide_book") {
+        guideBook.show();
+    };
+});
 */
