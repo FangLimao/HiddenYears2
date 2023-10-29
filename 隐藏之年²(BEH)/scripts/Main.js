9b36d662fe74167ae08021ce158da098da9eb3c7
方漓猫 <mucigames@outlook.com>
2023-10-29 13:28:38 +0800

    删除弃用物品

--- 隐藏之年²(BEH)/scripts/Main.js
+++ 隐藏之年²(BEH)/scripts/Main.js
@@ -1,12 +1,15 @@
 import { Effect, world } from "@minecraft/server";
+import { ActionFormData } from "@minecraft/server-ui";
 
-/**
- * @author:方漓猫
- * @copyright:MIT
- */
-world.beforeEvents.itemUse.subscribe(EventSource => {
-    const ID = EventSource.itemStack;
-    if (ID = 'hy:diamond_badge') {
-        EventSource.source.addEffect('health_boost', 114514)
+let QuestBookUI = new ActionFormData();
+QuestBookUI.title("任务书");
+QuestBookUI.body("这里是隐藏之年的任务书（WIP）");
+QuestBookUI.button('主任务');
+
+world.beforeEvents.chatSend.subscribe(EventSource => {
+    const Player = EventSource.sender
+    const Message = EventSource.message
+    if (Message == '/qb show') {
+        QuestBookUI.show(Player)
     }
-});
+})
