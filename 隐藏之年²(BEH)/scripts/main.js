777ca9a1d9ed730d3f05abea2b9b9bee96149845
方漓猫 <mucigames@outlook.com>
2023-12-31 18:56:24 +0800

    refactor：重构徽章的事件触发器

--- 隐藏之年²(BEH)/scripts/main.js
+++ 隐藏之年²(BEH)/scripts/main.js
@@ -6,5 +6,13 @@ world.afterEvents.itemUse.subscribe((use) => {
     case "hy:amethyst_sword":
       player.addEffect("poison", 15, { amplifier: 1 });
       break;
+    case "hy:copper_badge":
+      player.runCommandAsync("function api/gby.effect/copper");
+    case "hy:diamond_badge":
+      player.runCommandAsync("function api/gby.effect/diamond");
+    case "hy:gold_badge":
+      player.runCommandAsync("function api/gby.effect/gold");
+    default:
+      break;
   }
 });
