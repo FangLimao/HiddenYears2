c0ca1315d4249f214b524597164e2cc30cf667ec
方漓猫 <mucigames@outlook.com>
2024-01-01 09:46:48 +0800

    test：测试用sapi重构仿制工具

---
+++ 隐藏之年²(BEH)/scripts/hy2/texts.js
@@ -0,0 +1,6 @@
+export let messageImitationDamage1 = {
+  translate: "hy.message.imitation_damage.1",
+};
+export let messageImitationDamage2 = {
+  translate: "hy.message.imitation_damage.2",
+};
--- 隐藏之年²(BEH)/scripts/main.js
+++ 隐藏之年²(BEH)/scripts/main.js
@@ -1,8 +1,9 @@
 import { world } from "@minecraft/server";
+import { messageImitationDamage1 } from "hy2/texts.js";
 
-world.afterEvents.itemUse.subscribe((use) => {
-  const player = use.source;
-  const itemId = use.itemStack.typeId;
+world.afterEvents.itemUse.subscribe((event) => {
+  const player = event.source;
+  const itemId = event.itemStack.typeId;
   switch (itemId) {
     case "hy:copper_badge":
       player.addEffect("health_boost", 300, { amplifier: 2 });
@@ -13,7 +14,24 @@ world.afterEvents.itemUse.subscribe((use) => {
     case "hy:golden_badge":
       player.addEffect("health_boost", 600, { amplifier: 4 });
       break;
+    case "hy:bandage":
+      player.runCommandAsync("function gameplay/items/medicines/bandage");
+      break;
+    case "hy:medicine_pack":
+      player.runCommandAsync("function gameplay/items/medicines/medicine_pack");
+      break;
     default:
       break;
+  }
+});
+
+world.afterEvents.playerBreakBlock.subscribe((event) => {
+  const item = event.itemStackBeforeBreak;
+  const player = event.player;
+  let isImitationTools = item.hasTag("hy:imitation_tools");
+  if (isImitationTools === true) {
+      player.applyDamage(2);
+      player.sendMessage(messageImitationDamage1);
+    
   }
 });
