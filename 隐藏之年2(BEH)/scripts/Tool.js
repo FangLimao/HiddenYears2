8e65d926d4b81d4d7db50adfc0f8348613a3c623
方漓猫 <mucigames@outlook.com>
2024-03-01 20:16:20 +0800

    feat:为自定义工具加入了hy:custom_tools标签

--- 隐藏之年2(BEH)/scripts/Tool.js
+++ 隐藏之年2(BEH)/scripts/Tool.js
@@ -4,13 +4,13 @@ import * as hyapi from "@hy2/lib.js";
 world.afterEvents.playerBreakBlock.subscribe((event) => {
   const PLAYER = event.player;
   let ITEM = hyapi.getEquipmentItem(PLAYER);
-  if (ITEM?.hasTag("hy:damage1_break")){
+  if (ITEM?.hasTag("hy:custom_tools")){
     let NEW_ITEM = hyapi.consumeDurability(ITEM, 1, PLAYER);
     PLAYER?.getComponent("minecraft:equippable")?.setEquipment(
       EquipmentSlot.Mainhand,
       NEW_ITEM,
     );
-  }else if(ITEM?.hasTag("hy:damage2_break")){
+  }else if(ITEM?.hasTag("hy:custom_weapons")){
    let NEW_ITEM = hyapi.consumeDurability(ITEM, 2, PLAYER);
    PLAYER?.getComponent("minecraft:equippable")?.setEquipment(
       EquipmentSlot.Mainhand,
@@ -22,13 +22,13 @@ world.afterEvents.playerBreakBlock.subscribe((event) => {
 world.afterEvents.entityHitEntity.subscribe((event)=>{
  const PLAYER = event.damagingEntity;
  let ITEM = hyapi.getEquipmentItem(event.damagingEntity);
- if(ITEM?.hasTag("hy:damage1_attack")){
+ if(ITEM?.hasTag("hy:custom_weapons")){
    let NEW_ITEM = hyapi.consumeDurability(ITEM, 1, event.damagingEntity);
     PLAYER?.getComponent("minecraft:equippable")?.setEquipment(
       EquipmentSlot.Mainhand,
       NEW_ITEM)
  }
- if(ITEM?.hasTag("hy:damage2_attack")){
+ if(ITEM?.hasTag("hy:custom_tools")){
    let NEW_ITEM = hyapi.consumeDurability(ITEM, 2, event.damagingEntity);
     PLAYER?.getComponent("minecraft:equippable")?.setEquipment(
       EquipmentSlot.Mainhand,
