9d02a9ec067077c98e8972aaca288ae4eee02890
方漓猫 <mucigames@outlook.com>
2024-03-10 11:55:09 +0800

    feat(loot/trade):加入了所有信纸的获取方式

--- 隐藏之年2(BEH)/scripts/@quest/config.js
+++ 隐藏之年2(BEH)/scripts/@quest/config.js
@@ -1,56 +1,48 @@
 const myQuestAPI = {
-    system: {
-        inactive: "myQuestAPI is inactive", // will be displayed if MyQuestAPI is not active
-        enable: true, // Set to true to enable and false to disable
-		tag: "op" // tag who can see quests when system[enable = false] || /tag @s add op
+  system: {
+    inactive: "myQuestAPI is inactive", // will be displayed if MyQuestAPI is not active
+    enable: true, // Set to true to enable and false to disable
+    tag: "op", // tag who can see quests when system[enable = false] || /tag @s add op
+  },
+  getIdentifier: {
+    author: "星屹工作室", // Author Name
+    title: "§l「隐藏之年²」主线任务", // Title
+    description: "照着这本破旧的札记一步一步踏上旅途吧……", // Description
+  },
+  getQuest: {
+    isNamespace: {
+      isDefault: false, // Useless  for now. If true, will use default namespace
+      // so you don't need to write namespace anymore
+      namespace: "minecraft:", // Only work if "isDefault: true"
     },
-    getIdentifier: {
-        author: "星屹工作室", // Author Name
-        title: "§l「隐藏之年²」主线任务", // Title
-        description: "照着这本破旧的札记一步一步踏上旅途吧……" // Description
+    itemHand: "minecraft:book",
+    items: ["minecraft:copper_ingot"],
+    rewards: [["minecraft:apple", 10]],
+  },
+  getForm: {
+    title: {
+      enable: true, // Change to false to generate title automatically
+      title: [
+        // you don't need to fill this field if title[enable = true]
+        "Title here",
+      ],
     },
-    getQuest: {
-    	isNamespace: { 
-			isDefault: false,  // Useless  for now. If true, will use default namespace
-    							 	 // so you don't need to write namespace anymore
-			namespace: "minecraft:" // Only work if "isDefault: true"
-		},
-        itemHand: "minecraft:book",
-        items: [
-            "minecraft:copper_ingot"
-        ],
-        rewards: [
-        	["minecraft:apple", 10]
-        ]
+    description: {
+      description: ["Collect coal"],
+      complated: "Complated",
+      notComplated: "In progress",
     },
-    getForm: {
-    	title: {
-    		enable: true, // Change to false to generate title automatically
-    		title: [
-    			// you don't need to fill this field if title[enable = true]
-    			"Title here"
-    		]
-    	},
-    	description: {
-    		description: [
-    			"Collect coal"
-    		],
-    		complated: "Complated",
-    		notComplated: "In progress"
-    	},
-    	icon: {
-    		enable: true, // Set to true to enable and false to disable
-    		confirm: `textures/ui/confirm`,
-    		items: [
-    			"items/copper_ingot"
-			]
-    	},
-    	button: {
-        	back: "§l返回", // Button Back
-        	check: "§l检查并提交", // Button Check
-			about: "§l关于"  // Button About
-    	}
-    }
-}
+    icon: {
+      enable: true, // Set to true to enable and false to disable
+      confirm: `textures/ui/confirm`,
+      items: ["items/copper_ingot"],
+    },
+    button: {
+      back: "§l返回", // Button Back
+      check: "§l检查并提交", // Button Check
+      about: "§l关于", // Button About
+    },
+  },
+};
 
-export {myQuestAPI}
+export { myQuestAPI };
--- 隐藏之年2(BEH)/scripts/Quest.js
+++ 隐藏之年2(BEH)/scripts/Quest.js
@@ -1,34 +1,44 @@
 /*
-* dave.myQuestAPI.{..}
-* getItemHand
-* getItems
-* getFormTitle
-* getFromDesc
-* getFromButton
-*/
+ * dave.myQuestAPI.{..}
+ * getItemHand
+ * getItems
+ * getFormTitle
+ * getFromDesc
+ * getFromButton
+ */
 
-import {
-    system,
-    world
-} from "@minecraft/server";
-import {
-    ActionFormData,
-    ModalFormData
-} from '@minecraft/server-ui'
+import { system, world } from "@minecraft/server";
+import { ActionFormData, ModalFormData } from "@minecraft/server-ui";
 
 // Used to import variables from config.js
-import * as dave from '@quest/config.js';
+import * as dave from "@quest/config.js";
 
 const {
   system: { enable: systemEnable, tag: systemTag, inactive: systemInactive },
-  getIdentifier: { author: getAuthor, title: getTitle, description: getDescription },
+  getIdentifier: {
+    author: getAuthor,
+    title: getTitle,
+    description: getDescription,
+  },
   getQuest: { itemHand: getItemHand, items: getItems, rewards: getRewards },
   getForm: {
     title: { enable: getTitleEnable, title: getTitleTitle },
-    description: { description: getDescriptionDescription, complated: getDescriptionComplated, notComplated: getDescriptionNotComplated },
-    icon: { enable: getIconEnable, confirm: getIconConfirm, items: getIconItems },
-    button: { back: getButtonBack, check: getButtonCheck, about: getButtonAbout }
-  }
+    description: {
+      description: getDescriptionDescription,
+      complated: getDescriptionComplated,
+      notComplated: getDescriptionNotComplated,
+    },
+    icon: {
+      enable: getIconEnable,
+      confirm: getIconConfirm,
+      items: getIconItems,
+    },
+    button: {
+      back: getButtonBack,
+      check: getButtonCheck,
+      about: getButtonAbout,
+    },
+  },
 } = dave.myQuestAPI;
 
 world.beforeEvents.itemUse.subscribe((eventData) => {
@@ -42,7 +52,9 @@ world.beforeEvents.itemUse.subscribe((eventData) => {
     } else {
       const runCmd = player.runCommandAsync;
       runCmd(`tellraw @s {"rawtext":[{"text":"${systemInactive}\n\n "}]}`);
-      runCmd(`tellraw @s {"rawtext":[{"text":"===== ${getTitle} =====\n§eRunning: §r${systemEnable}\n§eVersion: §r${systemVersion}\n§eModule by: §r@${getAuthor}\n§eDev by: §r@abcdave"}]}`);
+      runCmd(
+        `tellraw @s {"rawtext":[{"text":"===== ${getTitle} =====\n§eRunning: §r${systemEnable}\n§eVersion: §r${systemVersion}\n§eModule by: §r@${getAuthor}\n§eDev by: §r@abcdave"}]}`,
+      );
     }
   }
 });
@@ -53,11 +65,19 @@ const formMain = (player) => {
   let COUNT = 0;
 
   for (const thisItems of getItems) {
-    const getTitleAuto = thisItems.split(/:(.*)/s)[1].replaceAll('_', ' ').toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+    const getTitleAuto = thisItems
+      .split(/:(.*)/s)[1]
+      .replaceAll("_", " ")
+      .toLowerCase()
+      .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
     const hasTag = player.hasTag(thisItems);
 
     const title = getTitleEnable ? getTitleAuto : getTitleTitle[COUNT];
-    const icon = getIconEnable ? (hasTag ? getIconConfirm : `textures/${getIconItems[COUNT]}`) : undefined;
+    const icon = getIconEnable
+      ? hasTag
+        ? getIconConfirm
+        : `textures/${getIconItems[COUNT]}`
+      : undefined;
     const button = hasTag ? title + "\n §2完成" : title;
 
     form.button(button, icon);
@@ -65,7 +85,7 @@ const formMain = (player) => {
     COUNT++;
   }
 
-  form.show(player).then(response => {
+  form.show(player).then((response) => {
     if (response.selection === 0) {
       formAbout(player);
     }
@@ -76,25 +96,47 @@ const formMain = (player) => {
 };
 
 const formSelection = (player, string) => {
-  const getItemName = getItems[string - 1].split(/:(.*)/s)[1].replaceAll('_', ' ').toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
-  const getRewardName = getRewards[string - 1][0].split(/:(.*)/s)[1].replaceAll('_', ' ').toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+  const getItemName = getItems[string - 1]
+    .split(/:(.*)/s)[1]
+    .replaceAll("_", " ")
+    .toLowerCase()
+    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+  const getRewardName = getRewards[string - 1][0]
+    .split(/:(.*)/s)[1]
+    .replaceAll("_", " ")
+    .toLowerCase()
+    .replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
   const getIsComplate = player.hasTag(getItems[string - 1]);
   const form2 = new ActionFormData().title(getTitle);
 
   if (getIsComplate) {
-    form2.body(`${getDescriptionDescription[string - 1]}\n\n§eObjective: §r${getItemName}\n§eReward: §r${getRewardName}\n§eComplated: §r${getDescriptionComplated}`);
+    form2.body(
+      `${
+        getDescriptionDescription[string - 1]
+      }\n\n§eObjective: §r${getItemName}\n§eReward: §r${getRewardName}\n§eComplated: §r${getDescriptionComplated}`,
+    );
   } else {
-    form2.body(`${getDescriptionDescription[string - 1]}\n\n§eObjective: §r${getItemName}\n§eReward: §r${getRewardName}\n§eStatus: §r${getDescriptionNotComplated}`);
+    form2.body(
+      `${
+        getDescriptionDescription[string - 1]
+      }\n\n§eObjective: §r${getItemName}\n§eReward: §r${getRewardName}\n§eStatus: §r${getDescriptionNotComplated}`,
+    );
   }
 
   form2.button(getButtonCheck).button(getButtonBack);
-  form2.show(player).then(response2 => {
+  form2.show(player).then((response2) => {
     if (!response2.isCanceled) {
       if (response2.selection === 0 && !player.hasTag(getItems[string - 1])) {
         for (let i = 0; i < 36; i++) {
-          const inventoryItem = player.getComponent("inventory").container.getItem(i);
+          const inventoryItem = player
+            .getComponent("inventory")
+            .container.getItem(i);
           if (inventoryItem && inventoryItem.typeId === getItems[string - 1]) {
-            player.runCommandAsync(`give @s ${getRewards[string - 1][0]} ${getRewards[string - 1][1]}`);
+            player.runCommandAsync(
+              `give @s ${getRewards[string - 1][0]} ${
+                getRewards[string - 1][1]
+              }`,
+            );
             player.addTag(getItems[string - 1]);
             break;
           }
@@ -108,8 +150,13 @@ const formSelection = (player, string) => {
 };
 
 const formAbout = (player) => {
-  const form = new ActionFormData().title(getTitle).body(`§e运行: §r${systemEnable}\n§e模组作者: §r@${getAuthor}\n§e本任务书基于 §r@abcdave 的 myQuestAPI开发而成`).button(getButtonBack);
-  form.show(player).then(response => {
+  const form = new ActionFormData()
+    .title(getTitle)
+    .body(
+      `§e运行: §r${systemEnable}\n§e模组作者: §r@${getAuthor}\n§e本任务书基于 §r@abcdave 的 myQuestAPI开发而成`,
+    )
+    .button(getButtonBack);
+  form.show(player).then((response) => {
     if (response.selection === 0) {
       formMain(player);
     }
